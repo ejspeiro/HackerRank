3b83ed0bbb591ad61b8adaa7f75066e17c8b4910
Eduardo Sanchez <eduardo@museality.com>
2018-01-02 14:28:20 +0100

    Solved all_domains/tutorials/10_days_of_statistics.

--- all_domains/tutorials/10_days_of_statistics/day_0-mean_median_and_mode.cpp
+++ all_domains/tutorials/10_days_of_statistics/day_0-mean_median_and_mode.cpp
@@ -1,35 +1,38 @@
+// Copyright 2017 Eduardo Sanchez.
 #include <iostream>
 #include <algorithm>
 #include <valarray>
+#include <limits>
+#include <utility>
 #include <unordered_map>
 
-int main () {
-
-  size_t nn{}; // Number of elements in the array.
+int main() {
+  size_t nn{};  // Number of elements in the array.
 
   std::cin >> nn;
 
-  std::valarray<float> numArray(nn); // Array to hold the values.
+  std::valarray<float> numArray(nn);  // Array to hold the values.
 
   for (size_t ii = 0; ii < nn; ++ii) {
-    float tmp{}; // Temporary floating-point variable for input.
+    float tmp{};  // Temporary floating-point variable for input.
     std::cin >> tmp;
     numArray[ii] = tmp;
   }
 
   // Compute the mean of the array.
 
-  // This can be done in O(nn). If the problem did not include computkng the
+  // This can be done in O(nn). If the problem did not include computing the
   // mean (which requires a partial sorting), we could have just add the values
   // while reading them, and divide said sum by a count of the read values, thus
   // solving the mean in constant memory and linear complexity.
-  float mean{};
+
+  float mean{};  // Mean of the collection of values.
 
   mean = numArray.sum()/static_cast<float>(nn);
 
   std::cout << mean << std::endl;
 
-  // Compute the median of the array.
+  // Compute the median of the array. Linear complexity.
 
   // nth_element is a partial sorting algorithm that rearranges elements in
   // [first, last) such that:
@@ -47,7 +50,7 @@ int main () {
   // (1) and const T& for (2) (it is a mutable iterator for (1), and a constant
   // iterator for (2)).
 
-  float median{}; // Median of the values.
+  float median{};  // Median of the values.
 
   if (nn%2 == 0) {
     // nn is even. Return the average of the 2 values in the center.
@@ -66,16 +69,12 @@ int main () {
 
   std::cout << median << std::endl;
 
-  // Compute the modes of the array.
+  // Compute the modes of the array. Linear complexity.
 
-  // An unordered_map stores key-value pairs with no order. Retrieval based on
-  // a given key happens in constant time. We can keep an unordered_map of each
-  // value and its occurrence to then check in linear time if the maximum
-  // number of appereances is not unique.
+  std::unordered_map<float, int> modes{};  // Map of values to appearances.
 
-  std::unordered_map<float, int> modes(nn); // UMap of values to appearances.
-
-  for (const float val: numArray) {
+  // Count number of appereances of each element. Linear complexity.
+  for (float val : numArray) {
     if (modes.find(val) != modes.cend()) {
       modes.at(val)++;
     } else {
@@ -83,13 +82,34 @@ int main () {
     }
   }
 
-  for (const auto &mode: modes) {
-    std::cout << mode.first << " : " << mode.second << std::endl;
+  // Detect if there are different numbers of appereances. Linear complexity.
+  auto minElem = std::min_element(modes.begin(),
+                                  modes.end(),
+                                  [] (std::pair<float, int> aa,
+                                      std::pair<float, int> bb) {
+                                        return aa.second < bb.second;
+                                      });
+  auto maxElem = std::max_element(modes.begin(),
+                                  modes.end(),
+                                  [] (std::pair<float, int> aa,
+                                      std::pair<float, int> bb) {
+                                        return aa.second < bb.second;
+                                      });
+  if (minElem->second == maxElem->second) {
+    // All the values have the same number of appereances. Choose the smallest.
+    float minValue{std::min_element(modes.begin(), modes.end())->first};
+    std::cout << minValue << std::endl;
+  } else {
+    // Values have different appereances.
+    // Compute the highest number of appereances.
+    int maxNumAppears = maxElem->second;
+    // Find smallest key with value equal to maxNumAppears. Linear complexity.
+    float minValue{std::numeric_limits<float>::max()};
+    for (const auto &keyValue : modes) {
+      if (keyValue.second == maxNumAppears && keyValue.first < minValue) {
+        minValue = keyValue.first;
+      }
+    }
+    std::cout << minValue << std::endl;
   }
-
-  auto minElem = std::min_element(modes.begin(), modes.end());
-  auto maxElem = std::max_element(modes.begin(), modes.end());
-
-  std::cout << (*minElem).first << std::endl;
-  std::cout << (*maxElem).first << std::endl;
 }
