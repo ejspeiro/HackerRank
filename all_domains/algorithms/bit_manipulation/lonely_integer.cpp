63e45c6e83fc8e4231a836284b995d973bd3a720
ejspeiro <ejspeiro@gmail.com>
2018-06-19 00:29:41 +0200

    Linted versions for solutions in algorithms/bit_manipulation/.

--- all_domains/algorithms/bit_manipulation/lonely_integer.cpp
+++ all_domains/algorithms/bit_manipulation/lonely_integer.cpp
@@ -1,24 +1,27 @@
+// Copyright 2018 Eduardo Sanchez
 #include <iostream>
 #include <iterator>
 #include <vector>
 #include <algorithm>
 
 int main() {
-  
-  int nn{}; // Size of the array.
-  
+  int nn{};  // Size of the array.
+
   std::cin >> nn;
-  
-  std::vector<int> num_occurrences_of(101, 0); // The array.
-  
+
+  std::vector<int> num_occurrences_of(101, 0);  // The array.
+
   for (int ii = 0; ii < nn; ++ii) {
-    int ee{}; // Current element.
+    int ee{};  // Current element.
+
     std::cin >> ee;
     num_occurrences_of[ee]++;
   }
-  
+
   int n1{1};
-  auto pos = std::find(num_occurrences_of.begin(), num_occurrences_of.end(), n1);
-  
+
+  auto pos = std::find(num_occurrences_of.begin(),
+                       num_occurrences_of.end(),
+                       n1);
   std::cout << std::distance(num_occurrences_of.begin(), pos);
 }
